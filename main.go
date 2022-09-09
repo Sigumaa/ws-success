3cac66d887aa297d25b16885200bbbac66479d9b
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-09-10 00:02:07 +0900

    feat: ws connection

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var (
+	upgrader = websocket.Upgrader{}
+)
+
+func hello(c echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer func(ws *websocket.Conn) {
+		err := ws.Close()
+		if err != nil {
+			c.Logger().Error(err)
+		}
+	}(ws)
+
+	// Write
+	err = ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+	if err != nil {
+		c.Logger().Error(err)
+	}
+	return nil
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Static("/", "./")
+	e.GET("/ws", hello)
+	e.Logger.Fatal(e.Start(":1323"))
+}
