f8461b09353be5aac2113ccd612a5e2a552f015e
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-09-10 10:17:54 +0900

    chore: wip

--- main.go
+++ main.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
-
-var (
-	upgrader = websocket.Upgrader{}
-)
-
-func hello(c echo.Context) error {
-	var ws *websocket.Conn
-	err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
-	if err != nil {
-		c.Logger().Error(err)
-	}
-	return nil
-}
-
-func connect(c echo.Context) error {
-	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-	if err != nil {
-		return err
-	}
-
-	// Write
-	err = ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
-	if err != nil {
-		c.Logger().Error(err)
-	}
-	return nil
-}
 
 func main() {
 	e := echo.New()
