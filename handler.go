61457ae8a11f1a62e98cd833846de44a9ab4c40b
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-09-10 23:45:11 +0900

    chore: comment

--- handler.go
+++ handler.go
@@ -6,10 +6,13 @@ import (
 	"net/http"
 )
 
+// upgrader は WebSocket のアップグレードを行うための構造体
 var upgrader = websocket.Upgrader{}
 var rooms = Room{}
 
 func ServeWs(c echo.Context) error {
+
+	// WebSocket にアップグレード
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
@@ -24,6 +27,7 @@ func ServeWs(c echo.Context) error {
 
 	client := &Client{Ws: ws}
 
+	// 新しい接続があったら保存する
 	rooms.AddClient(client)
 	for {
 		//
@@ -34,6 +38,8 @@ func ServeWs(c echo.Context) error {
 		// }
 		// ReadMessage() によって受け取ったメッセージを Publish() で配信する
 		msg := <-C1
+
+		// rooms に保存されている全てのクライアントにメッセージを送信する
 		rooms.Publish([]byte(msg))
 	}
 
