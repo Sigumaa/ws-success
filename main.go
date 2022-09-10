c9a6319b5ac2257297ad1e76a57d5b747371c75b
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-09-10 23:33:33 +0900

    chore: comment

--- main.go
+++ main.go
@@ -6,14 +6,22 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// C1 is a channel
 var C1 = make(chan string)
 
 func main() {
 
+	// Echo instance
 	e := echo.New()
+
+	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	// Routes
 	e.GET("/ws", ServeWs)
 	e.GET("/hello", Hello)
+
+	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
