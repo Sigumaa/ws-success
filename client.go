c2ff23146aca51b486e2d59f1a0b3281f55f9755
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-09-10 23:53:11 +0900

    chore: comment

--- client.go
+++ client.go
@@ -6,6 +6,7 @@ type Client struct {
 	Ws *websocket.Conn
 }
 
+// Send はクライアントにメッセージを送信する
 func (client *Client) Send(msg []byte) error {
 	return client.Ws.WriteMessage(websocket.TextMessage, msg)
 }
