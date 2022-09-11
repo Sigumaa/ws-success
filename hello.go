7f1f58556ff1766ac3bebecaad26432b067bfb5c
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-09-11 16:46:54 +0900

    chore: comment

--- hello.go
+++ hello.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"net/http"
 )
 
 func Hello(c echo.Context) error {
+
 	// Write
-	C1 <- "Hello が実行されました！"
-	return c.String(http.StatusOK, "Hello, World!")
+	// C1 <- "Hello が実行されました！"
+	//return c.String(http.StatusOK, "Hello, World!")
+
+	// 実際に使用する際は消す
+	return nil
 }
